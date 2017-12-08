b86096ebec428cd6f28ec539aca1f3fa1ac7ecbc
Braulio Betancourt <braulio.1796@gmail.com>
2017-12-08 16:53:25 -0600

    Agregar finalizado

--- src/java/com/crud/controller/AddController.java
+++ src/java/com/crud/controller/AddController.java
@@ -1,4 +1,3 @@
-
 package com.crud.controller;
 
 import com.crud.model.DBConnect;
@@ -6,13 +5,17 @@ import com.crud.model.Personas;
 import com.crud.model.PersonasValidaciones;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("add.htm")
 public class AddController {
+
     PersonasValidaciones personasValidaciones;
     private JdbcTemplate jdbcTemplate;
 
@@ -21,13 +24,27 @@ public class AddController {
         DBConnect conexion = new DBConnect();
         this.jdbcTemplate = new JdbcTemplate(conexion.connect());
     }
-    
-    @RequestMapping(method = RequestMethod.GET) 
+
+    @RequestMapping(method = RequestMethod.GET)
     public ModelAndView form() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("add");
         mav.addObject("personas", new Personas());
         return mav;
     }
-    
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ModelAndView form(@ModelAttribute("personas") Personas person, BindingResult result, SessionStatus status) {
+        this.personasValidaciones.validate(person, result);
+        if (result.hasErrors()) {
+            ModelAndView mav = new ModelAndView();
+            mav.setViewName("add");
+            mav.addObject("personas", new Personas());
+            return mav;
+        } else {
+            this.jdbcTemplate.update("INSERT INTO persona(nombre, telefono, correo, edad) VALUES (?,?,?,?)",
+                    person.getNombre(), person.getTelefono(), person.getCorreo(), person.getEdad());
+            return new ModelAndView("redirect:/home.htm");
+        }
+    }
 }
